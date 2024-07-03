dbfeb8c76331d30bd00f5a6373ee9908675db470
MateoMor <mateomorales5558@gmail.com>
2024-07-03 16:51:32 -0500

    postgres añadido

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 import express from "express";
+import { config } from "dotenv";
+import pg from "pg";
 
-console.clear()
+console.clear();
+
+config();
 
 const app = express();
-
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
 });
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/ping", async (req, res) => {
+    const result =await pool.query("SELECT NOW()"); // Le consulta la fecha actual a la base de datos
+    
+    return res.json(result.rows[0]);
+  });
+
 app.listen(3000);
-console.log("Server running on port 3000")
+console.log("Server running on port 3000");
