4c490027fc20c9d5f4218eea78d4bd95410ebcda
MateoMor <mateomorales5558@gmail.com>
2024-07-04 13:48:41 -0500

    Github request configured

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -1,31 +1,45 @@
 import { Octokit } from "@octokit/rest";
+import { config } from "dotenv";
 
-export const registerAdmin = async (req, res) => {
-    const { username, email, password } = req.body;
+// Cargar las variables de entorno
+config();
+
+// Verificar que la variable de entorno está cargada correctamente
+console.log(`GITHUB_TOKEN: ${process.env.GITHUB_TOKEN}`);
+
+// Configurar Octokit con el token de acceso personal
+const octokit = new Octokit({
+  auth: process.env.GITHUB_TOKEN,
+});
+
+// Prueba para verificar la autentcación de usuario
+/* (async () => {
+  try {
+    const { data } = await octokit.request('GET /user');
+    console.log('Authenticated User:', data);
+  } catch (error) {
+    console.error('Authentication Error:', error);
+  }
+})(); */
+
+export const getUsers = async (req, res) => {
+  const { username } = req.params;
+
+  console.log(`Requesting data for GitHub user: ${username}`);
   
-    try {
-      const passwordHash = await bcrypt.hash(password, 10);
-      //para crear un nuevo usuario debemos tener un modelo de usuario (en este caso es User)
-      const newUser = new User({
-        username,
-        email,
-        password: passwordHash, //le decimos que la contraseña es nuestra contraseña encriptada
-        role: "admin",
-      });
-      const userSaved = await newUser.save(); //guardamos el nuevo usuario en la base de datos
-      const token = await createAccessToken({ id: userSaved._id }); //creamos el token de la id
-      const tokenRole = await createAccessToken({ role: userSaved.role }); //creamos el token de la id
-      //envia una cookie con el token y el tokenROle
-      res.cookie("token", token);
-      res.cookie("tokenRole", tokenRole);
-  
-      res.status(201).json({
-        _id: userSaved._id,
-        username: userSaved.username,
-        email: userSaved.email,
-        role: userSaved.role,
-      });
-    } catch (e) {
-      res.status(500).json({ message: e.message });
-    }
-  };
+  try {
+    // Hacer una solicitud a la API de GitHub para obtener el usuario
+    const { data } = await octokit.users.getByUsername({ username });
+
+    res.status(200).json({
+      message: "Get user",
+      user: data,
+    });
+  } catch (error) {
+    console.error('Error fetching user from GitHub:', error);
+    res.status(500).json({
+      message: "Error fetching user",
+      error: error.message,
+    });
+  }
+};
--- src/routers/routes.js
+++ src/routers/routes.js
@@ -1,19 +1,14 @@
 import { Router } from "express"; //importamos Router de express que nos ayuda a crear las rutas
-/* import {
-    registerAdmin,
-    register,
-    login,
-    logout,
-    profile,
-    getUsers,
-  } from "../controllers/controllers.js"; //importamos las funciones de auth.controllers.js */
+import { getUsers } from "../controllers/controllers.js"; //importamos las funciones de auth.controllers.js
 
 const router = Router(); //Instanciamos Router
 
 router.get("/", (req, res) => {
-    console.log("Rutas de la api");
-    //creamos una ruta get para la raiz de nuestro servidor
-    res.send("Rutas de la api"); //enviamos un mensaje
-  });
+  console.log("Rutas de la api");
+  //creamos una ruta get para la raiz de nuestro servidor
+  res.send("Rutas de la api"); //enviamos un mensaje
+});
+
+router.get("/github/users/:username", getUsers);
 
 export default router; //Exportamos las rutas
